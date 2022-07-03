ec78f2bdd863e4a6076cc348d1f023e443c9717e
brightodedo <54002497+brightodedo@users.noreply.github.com>
2022-07-02 22:39:09 -0400

    better front end

--- lifetracker-ui/src/components/App/NotFound/NotFound.jsx
+++ lifetracker-ui/src/components/App/NotFound/NotFound.jsx
@@ -1,10 +1,12 @@
 import * as React from 'react'
 import './NotFound.css'
+import imgSrc from '../../../assets/tomioka.png'
 
 
 export default function NotFound(){
     return(
         <div className="notfound">
+                <img src={imgSrc} alt="" />
             <div className="not-found">
                 <h1> 404 </h1>
                 <h1> NOT FOUND </h1>
